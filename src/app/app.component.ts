2bfa7ecb3062f8e290b7ed4bf3b12ba778c3b4a9
Samuel Ziegler (Local) <notyou@disroot.org>
2023-01-03 19:05:08 +0100

    add+fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { MatSnackBar } from '@angular/material/snack-bar'
 
 @Component({
   selector: 'generator-root',
@@ -6,5 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+  characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  specials = "\.[]{}()<>*+-=!?^$|";
+  nums = '0123456789';
+
   title = 'Strong Password Generator';
+  faCopy = faCopy;
+  pass = "";
+  passlength = 16;
+  isSetToSpecials: boolean = false;
+  isSetToNums: boolean = false;
+
+  inputUpdate(inputChange: any) {
+    this.passlength = inputChange.value;
+  }
+  checkNumUpdate() {
+    if (this.isSetToNums)
+      this.isSetToNums = false;
+    else
+      this.isSetToNums = true;
+  }
+  checkSpecialUpdate() {
+    if (this.isSetToSpecials)
+      this.isSetToSpecials = false;
+    else
+      this.isSetToSpecials = true;
+  }
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  openSnackBar(message: string) {
+    if (this.pass.length > 0)
+      this.snackBar.open(message, '', { duration: 3000 });
+  }
+
+  generatePass() {
+    if (this.passlength < 0 || this.passlength > 128) return;
+    this.pass = "";
+    var curchars = this.characters;
+    if (this.isSetToNums) curchars += this.nums;
+    if (this.isSetToSpecials) curchars += this.specials;
+
+    var charactersLength = curchars.length;
+    for (var i = 0; i < this.passlength; i++) {
+      this.pass += curchars.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  }
+
+
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
 @NgModule({
   declarations: [
@@ -10,7 +16,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    FormsModule,
+    ClipboardModule,
+    MatSliderModule,
+    NoopAnimationsModule,
+    MatCheckboxModule,
+    MatCheckboxModule,
+    MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
